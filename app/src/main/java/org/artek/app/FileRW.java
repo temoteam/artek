8e35d3c6b5d431fe287b55219c626060b46b9d9a
Andrey <rooh1622@gmail.com>
2016-12-18 04:17:46 +0300

    Moved writeFile / readFile to the separate class FileRW.

---
+++ app/src/main/java/org/artek/app/FileRW.java
@@ -0,0 +1,77 @@
+package org.artek.app;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import static android.content.Context.MODE_PRIVATE;
+
+/**
+ * Created by rooh1 on 18-Dec-16.
+ */
+
+public final class FileRW {
+    private final String IO_LOG_TAG = "FileRW";
+    private Context context;
+
+
+    public FileRW(Activity activity){
+        this.context = activity.getApplicationContext();
+    };
+    public FileRW(Context context){
+        this.context = this.context;
+    };
+    public void writeFile(String FILENAME, String content) {
+        try {
+
+            // open stream to write data
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(context.openFileOutput(FILENAME, MODE_PRIVATE)));
+            // write data
+            bw.write(content);
+
+            // close stream
+            bw.close();
+            Log.d(IO_LOG_TAG, "File write");
+            Log.d(IO_LOG_TAG, content);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String readFile(String FILENAME) {
+        String content = "";
+        String str = "";
+        try {
+
+            // open stream to read data
+            BufferedReader br = new BufferedReader(new InputStreamReader(context.openFileInput(FILENAME)));
+
+            // read data
+            while ((str = br.readLine()) != null) {
+                Log.d(IO_LOG_TAG, str);
+                content = content + str;
+            }
+
+            // close stream
+            br.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content;
+    }
+
+}
+
